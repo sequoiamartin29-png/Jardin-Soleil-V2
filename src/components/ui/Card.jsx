7c633f407febb78f1dc64e9c647e57409c028259
sequoiamartin29-png <sequoiamartin29@gmail.com>
2026-07-03 14:38:03 -0400

    RC1 Phase 3 - Add Card Component

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Card({ children, style = {}, className = "" }) {
+  return (
+    <div
+      className={`card ${className}`}
+      style={{
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
